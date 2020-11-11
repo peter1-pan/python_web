1bbc102d127c0e0740f5cd228291a2526c8f8a0e
peter1-pan <2418944827@qq.com>
2020-11-11 12:43:20 +0800

    modify User.image ddl.length

--- www/models.py
+++ www/models.py
@@ -12,7 +12,7 @@ class User(Model):
     passwd = StringField(ddl='varchar(50)')
     admin = BooleanField()
     name = StringField(ddl='varchar(50)')
-    image = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
     created_at = FloatField(default=time.time)
 
 class Blog(Model):
